2e4ca54f0c3ceb0442ba036d9be3ed13ddd1083b
Lucas Figueiredo (EXT-Nokia) <lucas.figueiredo.ext@nokia.com>
2026-01-31 12:23:48 +0000

    detect visible vertices

---
+++ lua/constants.lua
@@ -0,0 +1,12 @@
+MAP_W = 16
+MAP_H = 16
+TILE = 8
+
+in_progress = 0
+start_end_game = 1
+game_over = 2
+
+left = 0 right = 1 up = 2 down = 3
+valid_moves = { left, right, up, down }
+
+map_sand_sprite = 16
